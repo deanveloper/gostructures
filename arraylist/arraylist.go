9cc47f1b2fbcad3971e2e66d0b217ce35dc293fa
Dean Bassett <dean@deanveloper.com>
2017-10-15 01:39:12 -0400

    added get/remove, changed uint to int for convenience

--- arraylist/arraylist.go
+++ arraylist/arraylist.go
@@ -17,16 +17,44 @@ func New() *List {
 	return NewWithCap(16)
 }
 
-// Returns an arraylist with any capacity that you wish
-func NewWithCap(capacity uint) *List {
+// Returns an arraylist with any capacity that you wish.
+// Panics if capacity is negative.
+func NewWithCap(capacity int) *List {
+	if capacity < 0 {
+		panic(ErrOutOfBounds)
+	}
 	newList := new(List)
 	newList.arr = make([]interface{}, 0, capacity)
 	return newList
 }
 
 // Gets the length of the arraylist.
-func (l *List) Len() uint {
-	return uint(len(l.arr))
+func (l *List) Len() int {
+	return len(l.arr)
+}
+
+// Gets the value at an index in the arraylist.
+//
+// Returns the value at the given index, or ErrOutOfBounds if
+// the given index is < 0, or >= l.Len()
+func (l *List) Get(index int) (interface{}, error) {
+	if index < 0 || index >= l.Len() {
+		return nil, ErrOutOfBounds
+	}
+
+	return l.arr[index], nil
+}
+
+// Gets the value at the given index in the arraylist.
+//
+// Returns the value at the given index, and panics if the
+// given index is < 0, or >= l.Len().
+//
+// The advantage of this method is that it only returns
+// one value, allowing for the method to be inlined in
+// things like if-statements.
+func (l *List) GetForce(index int) interface{} {
+	return l.arr[index]
 }
 
 // Add an element to the end of the list.
@@ -39,11 +67,12 @@ func (l *List) AddFirst(elem interface{}) {
 	l.AddAt(0, elem)
 }
 
-// Add an element at a specific index. Returns ErrOutOfBounds if index is too large.
-func (l *List) AddAt(index uint, elem interface{}) error {
-	if index > l.Len() {
+// Add an element at a specific index. Returns ErrOutOfBounds if index < 0 or >= l.Len().
+func (l *List) AddAt(index int, elem interface{}) error {
+	if index < 0 || index >= l.Len() {
 		return ErrOutOfBounds
 	}
+
 	l.arr = append(l.arr, nil)
 	l.shift(index, 1)
 
@@ -71,8 +100,8 @@ func (l *List) AddSliceFirst(elem []interface{}) {
 	l.AddSliceAt(0, elem)
 }
 
-func (l *List) AddSliceAt(index uint, elems []interface{}) error {
-	if index > l.Len() {
+func (l *List) AddSliceAt(index int, elems []interface{}) error {
+	if index < 0 || index >= l.Len() {
 		return ErrOutOfBounds
 	}
 
@@ -85,21 +114,41 @@ func (l *List) AddSliceAt(index uint, elems []interface{}) error {
 	return nil
 }
 
+// Removes the value at an index.
+// Returns (value, error) where value is the removed
+// value, and error is ErrOutOfBounds if the given index < 0 or >= l.Len()
+func (l *List) Remove(index int) (interface{}, error) {
+	if index < 0 || index >= l.Len() {
+		return nil, ErrOutOfBounds
+	}
+
+	value := l.arr[index]
+	l.shift(index+1, -1)
+	l.arr = l.arr[:len(l.arr)-1]
+
+	return value, nil
+}
+
+// Copies the list into a slice.
+func (l *List) AsSlice() []interface{} {
+	result := make([]interface{}, l.Len())
+	copy(result, l.arr)
+	return result
+}
+
 // === PRIVATE METHODS ===
 
 // Copies the array into itself with an offset of shamt.
 // Uses the copy() builtin to do so. This method assumes that
 // l.arr is large enough.
-func (l *List) shift(start uint, shamt int) {
+func (l *List) shift(start, shamt int) {
 	if shamt == 0 {
 		return
 	}
 
-	iStart := int(start)
-
 	if shamt > 0 {
-		copy(l.arr[iStart+shamt:], l.arr[start:len(l.arr)-shamt+1])
+		copy(l.arr[start+shamt:], l.arr[start:len(l.arr)-shamt+1])
 	} else {
-		copy(l.arr[iStart+shamt:], l.arr[start:len(l.arr)])
+		copy(l.arr[start+shamt:], l.arr[start:len(l.arr)])
 	}
 }
