bd65281af87f9a140998d2663bf43417142c8f87
Dean Bassett <dean@deanveloper.com>
2017-10-15 02:19:57 -0400

    added a mega test which tests everything

--- arraylist/arraylist_test.go
+++ arraylist/arraylist_test.go
@@ -38,3 +38,59 @@ func TestSingle(t *testing.T) {
 		t.Errorf("AsSlice(): Expected %v, Actual %v", expectedSlice, list.AsSlice())
 	}
 }
+
+func TestMega(t *testing.T) {
+	list := New()
+	list.AddLast("Last")
+	list.Remove(0)
+
+	val, err := list.Get(0)
+	if err != ErrOutOfBounds || val != nil {
+		t.Fatalf("Remove -> Get: Expected (val %v, err %v), Actual (val %v, err %v)", nil, ErrOutOfBounds, val, err)
+	}
+
+	list.AddLast(1)
+	list.AddLast(2)
+	list.AddLast(3)
+	list.AddLast("four")
+	list.AddLast("FiVe")
+	list.AddFirst("zero")
+
+	if list.Len() != 6 {
+		t.Errorf("Len(): Expected %d, Actual %d", 6, list.Len())
+	}
+
+	expectedSlice := []interface{}{"zero", 1, 2, 3, "four", "FiVe"}
+	if !reflect.DeepEqual(list.AsSlice(), expectedSlice) {
+		t.Errorf("Slices: Expected %v, Actual %v", expectedSlice, list.AsSlice())
+	}
+
+	list.Remove(2)
+	expectedSlice2 := []interface{}{"zero", 1, 3, "four", "FiVe"}
+	if !reflect.DeepEqual(list.AsSlice(), expectedSlice2) {
+		t.Errorf("Slices: Expected %v, Actual %v", expectedSlice2, list.AsSlice())
+	}
+
+	list.Remove(0)
+	expectedSlice3 := []interface{}{1, 3, "four", "FiVe"}
+	if !reflect.DeepEqual(list.AsSlice(), expectedSlice3) {
+		t.Errorf("Slices: Expected %v, Actual %v", expectedSlice3, list.AsSlice())
+	}
+
+	list.Remove(3)
+	expectedSlice4 := []interface{}{1, 3, "four"}
+	if !reflect.DeepEqual(list.AsSlice(), expectedSlice4) {
+		t.Errorf("Slices: Expected %v, Actual %v", expectedSlice4, list.AsSlice())
+	}
+
+	list.AddFirst(nil)
+	list.AddFirst(10)
+	expectedSlice5 := []interface{}{10, nil, 1, 3, "four"}
+	if !reflect.DeepEqual(list.AsSlice(), expectedSlice5) {
+		t.Errorf("Slices: Expected %v, Actual %v", expectedSlice5, list.AsSlice())
+	}
+	val2, err2 := list.Get(1)
+	if val2 != nil || err2 != nil {
+		t.Errorf("Get nil: Expected (val %v, err %v), Actual (val %v, err %v)")
+	}
+}
